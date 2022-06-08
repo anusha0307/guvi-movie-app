417249b968ebb97adb1b9fba3089fe41955f7bf3
anusha0307 <bellamkondaanusha34@gmail.com>
2022-06-08 12:08:33 +0530

    message list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 
@@ -12,8 +12,40 @@ import './App.css';
 //webpack +babel is used to convert the jsx into javascript
 
 
-
+// //1
 // function App() {
+//   //js starts
+//   const name="anusha";
+//   const time=30;
+  
+//   //js ends
+//   //jsx starts
+//   return (
+    
+//     <div className="App">
+
+//       <h1>hello,{name}</h1>  
+//       <p>time to complete -{5*time}mins</p>
+//       <h2>start editing to see some magic happen!!</h2>
+//     </div>
+//   );
+//   //jsx ends
+// }
+
+// export default App;
+
+//day02
+//how to create component
+// how function become component
+//1. the first letter to be capital
+// 2. it should return one jsx element bt it should nt be more than one jsx element
+//<> </>--->react fragment helps in styling  and perfomance.
+
+//define the component
+//the message component is called inside the app component
+
+//2----
+// function Appa() {
 //   //js starts
 //   const name="anusha";
 //   const time=30;
@@ -33,18 +65,7 @@ import './App.css';
 //   //jsx ends
 // }
 
-// export default App;
-
-// //day02
-// //how to create component
-// // how function become component
-// //1. the first letter to be capital
-// // 2. it should return one jsx element bt it should nt be more than one jsx element
-// //<> </>--->react fragment helps in styling  and perfomance.
-
-// //define the component
-// //the message component is called inside the app component
-
+// export default Appa;
 
 // function Message(props){
 //   // const name="kalyani"
@@ -54,9 +75,10 @@ import './App.css';
 //   <h1>hiiiiiiii,{props.nam}....</h1>
 //   </div>
 //   )
-// }
+// }------
 
 
+//3.---
 // // using map function
 // function App() {
 //   const names=["anushs","kalyani","jansi"]
@@ -75,45 +97,42 @@ import './App.css';
 //   <h1>hello....{props.name}</h1>
 //   </>
 // )
+// }---
+
+//4----
+// usin map for  objrect
+// function App() {
+//   // const names=["anushs","kalyani","jansi"]
+//   const students= [
+//     { 
+//       name:"anu",
+//       pic:"https://tse2.mm.bing.net/th?id=OIP.vJ3aFoQV6wd99bwSXpENJAAAAA&pid=Api&P=0&w=102&h=154" 
+//    },
+//   {  name:"kalyani",
+//      pic:"https://tse2.mm.bing.net/th?id=OIP.YEMKk8DvRpM4-lIo2nQHXgAAAA&pid=Api&P=0&w=142&h=213"  
+// },
+// {
+//   name:"jansi",
+//   pic:"https://tse4.mm.bing.net/th?id=OIP.tdfjQXRfxuaAwaFDrNXATwHaEK&pid=Api&P=0&w=326&h=183 "
 // }
+// ]
+//   return(
+//     <div className="App">
 
-function App() {
-  // const names=["anushs","kalyani","jansi"]
-  const students= [
-    { 
-      name:"anu",
-      pic:"https://tse2.mm.bing.net/th?id=OIP.vJ3aFoQV6wd99bwSXpENJAAAAA&pid=Api&P=0&w=102&h=154" 
-   },
-  {  name:"kalyani",
-     pic:"https://tse2.mm.bing.net/th?id=OIP.YEMKk8DvRpM4-lIo2nQHXgAAAA&pid=Api&P=0&w=142&h=213"  
-},
-{
-  name:"jansi",
-  pic:"https://tse4.mm.bing.net/th?id=OIP.tdfjQXRfxuaAwaFDrNXATwHaEK&pid=Api&P=0&w=326&h=183 "
-}
-]
-  return(
-    <div className="App">
+//     {students.map((student)=>(<Welcome   name={student.name}  pics={student.pic} />))}
+//     </div>
+//   )
+// }
+// export default App;
 
-    {students.map((student)=>(<Welcome   name={student.name}  pics={student.pic} />))}
-    </div>
-  )
-}
-export default App;
-
-//defined the component--logic +view
-function Welcome(props){
-return(
-  <>
-  <h1>hello....{props.name}</h1>
-  <img src={props.pics} />
-  </>
-)
-}
+// //defined the component--logic +view
+// function Welcome(props){
+// return(
+//   <>
+//   <h1>hello....{props.name}</h1>
+//   <img src={props.pics} />
+//   </>
+// )
+// }----
 
 
-
-
-// {Pics.map((nm)=>(<Welcome pic={nm} />))}
-// const Pics=[" https://tse4.mm.bing.net/th?id=OIP.6MU1k5fzXx74_zd3BEIczQHaEo&pid=Api&P=0&w=278&h=174 ","https://tse2.mm.bing.net/th?id=OIP.fuKDmQHzlzunZBM2dzAZJwHaEK&pid=Api&P=0&w=334&h=188 "," https://tse1.mm.bing.net/th?id=OIP.j1Yfs1AOFhRIhr12zmx76gHaEo&pid=Api&P=0&w=259&h=161"]
-// <img src={props.pic} />
